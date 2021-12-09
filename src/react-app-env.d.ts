86d32c61c02ba5cbef669dade86e818e694976e5
mdarche <mkdarche@gmail.com>
2021-12-08 20:59:11 -0500

    Flatten project directories and move svgs to /src

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react-scripts" />
+
+interface Traits {
+  base: 'Devie' | 'Maggie' | 'Johnny' | 'Swervie';
+  arms: string;
+  chest: string;
+  color: string;
+  background: string;
+}
